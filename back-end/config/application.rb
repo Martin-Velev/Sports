66fa0e02ba7a58e956ed519994d64476facc7cd7
Jojo Nedich <jojo.nedich@gmail.com>
2019-09-23 17:28:52 +0300

    BE: allow calls from localhost

--- back-end/config/application.rb
+++ back-end/config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -33,5 +33,13 @@ module BackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Allow calls from localhost:3000
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "http://localhost:3000"
+        resource "*", :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
+      end
+    end
   end
 end
