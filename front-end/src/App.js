98e15f89936e2e36648884c85b50c1067c7c6b5f
Jojo Nedich <jojo.nedich@gmail.com>
2019-09-23 17:19:59 +0300

    FE: set up routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import React, { Component } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/sports" exact component={Sports} />
+      <Route path={`/sports/:sportId`} exact component={Events} />
+      <Route path={`/sports/:sportId/events/:eventId`} component={Outcomes} />
+    </Router>
   );
 }
 
