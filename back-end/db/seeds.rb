6a4044e7c9fdc6a806845a8c8919dad9f080eba2
Jojo Nedich <jojo.nedich@gmail.com>
2019-09-23 16:36:11 +0300

    BE: Seed json in to database

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seed the database with info from the JSON file
+
+require "json"
+
+Outcome.delete_all
+Event.delete_all
+Sport.delete_all
+
+file = File.read("./db/sports.json")
+data = JSON.parse file
+
+sports = data["sports"]
+
+sports.each do |sport|
+  events = []
+  sport["comp"].each do |comp|
+    comp["events"].each do |event|
+      score = event["scoreboard"]
+      outcome = Outcome.create(id: score["eId"], scoreA: score["scrA"], scoreB: score["scrB"])
+      event = Event.create(
+        id: event["id"],
+        outcome: outcome,
+        description: event["desc"],
+        teamA: event["oppADesc"],
+        teamB: event["oppBDesc"],
+      )
+      events << event
+    end
+  end
+  Sport.create(id: sport["id"], events: events, description: sport["desc"])
+end
